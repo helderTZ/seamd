7123db90617923633dbaeba28683daa19fd9b749
Helder Duarte <heldertz2@gmail.com>
2025-07-13 20:55:04 +0100

    Make highlighting parallel and add execution time measurement

--- src/carve.rs
+++ src/carve.rs
@@ -1,27 +1,29 @@
 use crate::seam::FullSeam;
 use image::{ImageBuffer, Rgb};
+use rayon::prelude::*;
 
 pub fn highlight_seam(
-    img_buf: &ImageBuffer<Rgb<u8>, Vec<u8>>,
+    img_buf: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
     seam: &FullSeam,
-) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    let (w, h) = img_buf.dimensions();
-    let mut highlighted_img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(w, h);
+    iteration: i32
+) {
+    img_buf.par_enumerate_pixels_mut().for_each(|(i,j, pixel)| {
+        if j > 0 && (i + iteration as u32) as usize == seam.seam_path[(j-1) as usize].from {
+            //TODO: skip col when hit seam
+            *pixel = Rgb([255, 0, 0]);
+        }
+    });
 
     // should this go from new_h-1 to 0 ?
-    for j in 0..(h - 1) as usize {
-        for i in 0..(w - 1) as usize {
-            if j > 0 && i == seam.seam_path[j - 1].from {
-                //TODO: skip col when hit seam
-                *highlighted_img.get_pixel_mut(i as u32, j as u32) = Rgb([255, 0, 0]);
-            } else {
-                *highlighted_img.get_pixel_mut(i as u32, j as u32) =
-                    *img_buf.get_pixel(i as u32, j as u32);
-            }
-        }
-    }
-
-    highlighted_img
+    // let (w, h) = img_buf.dimensions();
+    // for j in 0..(h - 1) as usize {
+    //     for i in 0..(w - 1) as usize {
+    //         if j > 0 && i == seam.seam_path[j - 1].from {
+    //             //TODO: skip col when hit seam
+    //             *img_buf.get_pixel_mut(i as u32 + iteration as u32, j as u32) = Rgb([255, 0, 0]);
+    //         }
+    //     }
+    // }
 }
 
 pub fn carve_seam(
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::{io::Write, time::{SystemTime, UNIX_EPOCH}};
 use clap::Parser;
 
 mod carve;
@@ -26,9 +26,13 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    let highlight_file = format!("highlight_{}", args.output_file);
 
-    println!("Original file: {}", args.input_file);
+    println!("Input file: {}", args.input_file);
     println!("Output file: {}", args.output_file);
+    if args.seam_highlight {
+        println!("Highlighted file: {}", highlight_file);
+    }
     println!("Number of seams to carve: {}", args.nr_seams);
     println!("Highlight enabled: {}", args.seam_highlight);
 
@@ -41,17 +45,27 @@ fn main() {
 
     let mut highlighted_img = img_buf.clone();
 
+    let mut seam_carving_timings = Vec::with_capacity(args.nr_seams as usize);
+    let mut seam_highlight_timings = Vec::with_capacity(args.nr_seams as usize);
+
     for s in 0..args.nr_seams {
         print!("\rCarving seam {}/{} ...", s+1, args.nr_seams);
         let _ = std::io::stdout().flush();
 
+        let start_seam_carving = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
         let seams = seam::get_seams(&img_buf);
         let min_seam = seam::get_min_seam(&seams);
         let carved_img = carve::carve_seam(&img_buf, min_seam);
+        let end_seam_carving = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        seam_carving_timings.push(end_seam_carving - start_seam_carving);
+
         img_buf = carved_img.clone();
 
         if args.seam_highlight {
-            highlighted_img = carve::highlight_seam(&highlighted_img, min_seam);
+            let start_seam_highlight = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+            carve::highlight_seam(&mut highlighted_img, min_seam, s);
+            let end_seam_highlight = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+            seam_highlight_timings.push(end_seam_highlight - start_seam_highlight);
         }
 
         if s == args.nr_seams-1 {
@@ -59,11 +73,26 @@ fn main() {
             .expect("could not save carved image");
 
             if args.seam_highlight {
-                highlighted_img.save(format!("highlight_{}", args.output_file))
+                highlighted_img.save(&highlight_file)
                     .expect("could not save highlighted image");
             }
         }
     }
-
     println!();
+
+    let seam_carving_avg = seam_carving_timings.iter().map(|d| d.as_millis()).sum::<u128>() as f64 / seam_carving_timings.len() as f64;
+    let seam_carving_max = seam_carving_timings.iter().map(|d| d.as_micros()).max().unwrap_or(0) as f64 * 1e-3;
+    let seam_carving_min = seam_carving_timings.iter().map(|d| d.as_micros()).min().unwrap_or(0) as f64 * 1e-3;
+    println!("Average seam carving time: {}ms", seam_carving_avg);
+    println!("Maximum seam carving time: {}ms", seam_carving_max);
+    println!("Minimum seam carving time: {}ms", seam_carving_min);
+    
+    if args.seam_highlight {
+        let seam_highlight_avg = seam_highlight_timings.iter().map(|d| d.as_millis()).sum::<u128>() as f64 / seam_highlight_timings.len() as f64;
+        let seam_highlight_max = seam_highlight_timings.iter().map(|d| d.as_micros()).max().unwrap_or(0) as f64 * 1e-3;
+        let seam_highlight_min = seam_highlight_timings.iter().map(|d| d.as_micros()).min().unwrap_or(0) as f64 * 1e-3;
+        println!("Average seam highlight time: {}ms", seam_highlight_avg);
+        println!("Maximum seam highlight time: {}ms", seam_highlight_max);
+        println!("Minimum seam highlight time: {}ms", seam_highlight_min);
+    }
 }
